5edcc7384f195dbebde5420a760fce30624475f8
Adam Mason <60157744+admason@users.noreply.github.com>
2020-06-07 20:49:48 +0100

    Update Just_Krill.m

--- Just_Krill.m
+++ Just_Krill.m
@@ -1,58 +1,110 @@
 %n=1
+
 k(1)=0.1;
+
 r(1)=0.1;
+
 ni=1;
-for n=2:1
+
+for n=2:10
+
 	k(n)=k(n-1)+r(1)*k(n-1)*(1-k(n-1));
+
 end
+
 subplot(221), plot(k)
-title(['Krill population, n=1  '])
+
+title(['Krill population, n=10  '])
+
     xlabel('Iterations');
+
     ylabel('Population as proportion [0,1]');
-    xlim([0 100])
+
+    xlim([0 10])
+
     ylim([0 1.1])
+
 hold on
+
+
+
 
 
 %n=2%
-for n=2:2
+
+for n=2:50
+
     k(n)=k(n-1)+r(1)*k(n-1)*(1-k(n-1));
+
 end
+
 subplot(222),plot(k)
-title(['Krill population, n=2 '])
+
+title(['Krill population, n=50 '])
+
     xlabel('Iterations');
+
     ylabel('Population as proportion [0,1]');
-    xlim([0 100])
+
+    xlim([0 50])
+
     ylim([0 1.1])
+
 hold on
+
+
+
 
 
 k(1)=0.1;
+
 r(1)=0.1;
+
 ni=1;
-for n=2:5000
+
+for n=2:75
+
     k(n)=k(n-1)+r(1)*k(n-1)*(1-k(n-1));
+
 end
+
 subplot(223),plot(k)
-title(['Krill population, n=5000'])
+
+title(['Krill population, n=75'])
+
     xlabel('Iterations');
+
     ylabel('Population as proportion [0,1]');
+
     xlim([0 100])
+
     ylim([0 1.1])
+
 hold on
+
+
 
 %n=100%
-for n=2:100
-    k(n)=k(n-1)+r(1)*k(n-1)*(1-k(n-1));
-end
-subplot(224),plot(k)
-title(['Krill population, n=100 '])
-    xlabel('Iterations');
-    ylabel('Population as proportion [0,1]');
-    xlim([0 100])
-    ylim([0 1.1])
-hold on
 
+for n=2:100
+
+    k(n)=k(n-1)+r(1)*k(n-1)*(1-k(n-1));
+
+end
+
+subplot(224),plot(k)
+
+title(['Krill population, n=100 '])
+
+    xlabel('Iterations');
+
+    ylabel('Population as proportion [0,1]');
+
+    xlim([0 100])
+
+    ylim([0 1.1])
+
+hold on
 
 
 
